60aecb7078030b3ebcf57e0e1d0f4a347d64265a
Tita Navarro <45797993+Tita-Navarro@users.noreply.github.com>
2019-04-30 09:49:31 -0500

    adding new functions without testing

--- md-links.js
+++ md-links.js
@@ -1,7 +1,11 @@
 //file system
-
 const fs = require('fs');
+//route
 const path = require('path');
+
+const cheerio = require ('cheerio');
+
+const markdown = require ('markdown').markdown;
 
 module.exports = {
     validatePath: function(mdFile) {
@@ -13,6 +17,20 @@ module.exports = {
     console.log('Tu archivo no es .md');
     return false;
   }
+},
+fileExist: function(mdFile){
+    //console.log(fileExist, mdFile);
+    return new Promise((resolve, reject)=>{
+        fs.existsSync(mdFile, function(exists){
+            if (exists){
+                console.log("the file exists");
+                resolve(true);
+            } else{
+                console.log("the file does not exist");
+                reject (false);
+            }
+        })
+    }) 
 },
 absolutePath: function(pathRoute) {
   const absolute = path.resolve(pathRoute);
@@ -29,47 +47,24 @@ readingFile: function(mdFile){
     });
 
   });
+ 
 },
-
-fileExist: function(mdFile){
-    console.log(fileExist, mdFile);
-    return new Promise((resolve, reject)=>{
-        fs.existsSync(mdFile, function(exists){
-            if (exists){
-                console.log("the file exists");
-                resolve(true);
-            } else{
-                console.log("the file does not exist");
-                reject (false);
-            }
-        })
-    }) 
+htmlConverter: function (data, path){
+    return fileHTML = markdown.toHTML(data);
 },
+arrays: function (fileHTML, path){
+let links = [],
+let link = {},
 
+let $ = cheerio.load(fileHTML);
+$('a').each(function(){
+  link = { href: $(this).attr('href'),
+           text: $(this).text(),
+           file: path
+},
+links.push(link);
+});
+return (links);
+},
 }
 
-/*module.exports = {
-    pathTrue: function (pathFile){
-        if (pathFile!=undefined){
-            console.log('Ingresaste una ruta de archivo');
-            return true;
-        } else{
-            console.log('Ingresa una ruta');
-            return false;
-        }
-    },
-    //the file exists
-    existFile: function (pathFile){
-        return new Promise((resolve, reject)=>{
-            fs.existsSync(pathFile, function(exists){
-                if(exists){
-                    console.log('The file exists');
-                    resolve(true);
-                } else{
-                    console.log('the file does not exist');
-                    resolve(false);
-                }
-            });
-        })
-    },
-}*/
