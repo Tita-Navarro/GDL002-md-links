1918cc40b1579d415a1956764c4bc3524939362f
Tita Navarro <45797993+Tita-Navarro@users.noreply.github.com>
2019-05-07 09:01:05 -0500

    adding test into master branch

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,21 +1,53 @@
-const mdLinks = require('../src/md-links.js');
+const {validatingPath, absoluteOrRelative, fileExist, validatePath, dirOrFile} = require('../md-links.js');
+const Directory = 'C:\\Users\\tita_\\Documents\\proyecto-laboratoria\\GDL002-md-links\\README.md';
+const File = 'README.md';
 
-test('should be a markdown file', () =>{
-  expect(mdLinks.validatePath("README.md")).toBe(true);
+test('should be a valid file', () =>{
+  expect(validatingPath(File)).toBe(true);
+});
+
+test('Should return false to validate path', () => {
+  expect(validatingPath()).toBe(false);
 });
 
 test('should be an absolute route', ()=>{
-  expect(mdLinks.absolutePath("README.md")).toBe("C:\\Users\\tita_\\Documents\\proyecto-laboratoria\\GDL002-md-links\\README.md");
+  expect(absoluteOrRelative('README.md')).toBe('C:\\Users\\tita_\\Documents\\proyecto-laboratoria\\GDL002-md-links\\README.md');
 });
 
-test('should read the file', ()=>{
-  mdLinks.readingFile("./test/prueba.md").then((result) =>{
+test('Should be a relative path', ()=>{
+  expect(absoluteOrRelative('README.md')).toBe('C:\\Users\\tita_\\Documents\\proyecto-laboratoria\\GDL002-md-links\\README.md');
+});
+
+test('Shoul be a ".md" file', () => {
+  expect(validatePath('prueba.txt')).toBe(false);
+});
+
+test('Shoul be a ".md" file', () => {
+  expect(validatePath('README.md')).toBe(true);
+});
+
+/*test('should read the file', ()=>{
+  readingFile("./test/prueba.md").then((result) =>{
     expect(result).tobe('hola');
   })
-});
+});*/
 
 test ('file should exist', ()=>{
-  mdLinks.fileExist("README.md").then(result =>{
+  fileExist("README.md").then(result =>{
     expect(result).toBe(true);
   })
+});
+
+test('Should show the name of a directory', () => {
+  dirOrFile(File).then(result => {
+    expect(result).toBe(Directory);
+  });
+});
+
+test('Should show the name of a directory', () => {
+  dirOrFile(File).then(result => {
+    expect(result)
+      .contains(`✔ ${File}`)
+      .cath(error);
+  });
 });
